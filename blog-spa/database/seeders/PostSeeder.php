00b1f1b5326b5f01794b1ee1f298182c5aeb3ccf
Michael <mickeymakesapps88@gmail.com>
2021-09-30 18:11:36 -0700

    complete posts table and migration with seeds

---
+++ blog-spa/database/seeders/PostSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        $faker = Faker::create();
+
+        foreach(range(1, 10) as $index) {
+
+            DB::table('posts')->insert([
+                'name' => $faker->sentence(5),
+                'description' => $faker->paragraph(2),
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ]);
+
+        }
+        
+    }
+}
